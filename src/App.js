975487dd52b298c8f6ec57aab0923697acc86f4a
Gaurav <mohapatragaurav9@gmail.com>
2018-06-04 19:31:51 +0530

    Convert csv to object

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    data: []
+  };
+
+  convertCsvtoObject = csv => {
+    const lines = csv.split("\n");
+    const [headerText] = lines.splice(0, 1);
+    const headers = headerText.split(",");
+    const result = lines.map(line => {
+      const items = line.split(",");
+      return items.reduce((accum, item, index) => {
+        const key = headers[index];
+        accum[key] = item;
+        return accum;
+      }, {});
+    });
+    return result;
+  };
+
   componentWillMount() {
     fetch(
       "https://raw.githubusercontent.com/radik909/cart/master/src/MOCK_DATA.csv"
     )
       .then(res => res.text())
       .then(csv => {
-        console.log(csv);
+        this.setState({
+          data: this.convertCsvtoObject(csv)
+        });
       });
   }
+
   render() {
+    console.log(this.state.data);
     return (
       <div className="App">
         <header className="App-header">
